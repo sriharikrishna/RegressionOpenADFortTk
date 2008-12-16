c32e9876990b8debb7630b72b417a7075e1d3a68
m_utkej <m_utkej@utke2.site>
2008-12-16 13:55:35 -0600

    lost changes

--- Reference/arrays1.x2w.w2f.f
+++ Reference/arrays1.x2w.w2f.f
@@ -5,7 +5,6 @@
 C
 C     **** Local Variables and Functions ****
 C
-      INTEGER(w2f__i4) I
       INTEGER(w2f__i4) II
       INTEGER(w2f__i4) IX(1 : 10)
       REAL(w2f__8) X(1 : 20)
@@ -13,17 +12,12 @@ C
 C
 C     **** Statements ****
 C
-1     CONTINUE
-      GO TO 2
-2     CONTINUE
       X(1 : 20) = 2.5
       Y = 3.5D00
       IX(1 : 10) = 5
-      I = 10
+      II = 10
       X(1) = (X(1) * X(4) + 3.14149999618530273438D00 - Y)
       IX(4) = (II +(IX(1) / IX(2)))
-      WRITE(*, *) 'x(1) = ', X(1), '"ix(4)" = ', IX(4)
+      WRITE(*, *) 'x(1) = ', X(1), 'ix(4) = ', IX(4)
       
-      GO TO 3
-3     CONTINUE
       END PROGRAM
