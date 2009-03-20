99cd9fd685147b9e65f65d1d612d8a81a26921ec
m_utkej <m_utkej@utke2.site>
2009-03-20 11:49:49 -0500

    after the fix

---
+++ Reference/PostProcess_activeInterface.x2w.w2f.f
@@ -0,0 +1,50 @@
+
+      SUBROUTINE foo(X)
+      use w2f__types
+      use OAD_active
+      IMPLICIT NONE
+C
+C     **** Parameters and Result ****
+C
+      type(active) :: X
+C
+C     **** Statements ****
+C
+      X%v = (X%v*2.0D00)
+      END SUBROUTINE
+
+      PROGRAM activeinterface
+      use w2f__types
+      use OAD_active
+      IMPLICIT NONE
+C
+C     **** Local Variables and Functions ****
+C
+      type(active) :: X(1:2)
+      type(active) :: Y(1:2)
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+      interface 
+        SUBROUTINE foo(X)
+        use w2f__types
+      use OAD_active
+        type(active) :: X
+        END SUBROUTINE
+
+      end interface 
+      
+      X(1)%v = 2.0
+      X(2)%v = 3.0
+      CALL foo(X(1))
+      Y(1:2)%v = X(1:2)%v
+      IF (Y(1)%v.eq.4.0D00) THEN
+        WRITE(*, *) 'OK'
+      ENDIF
+      
+      END PROGRAM
