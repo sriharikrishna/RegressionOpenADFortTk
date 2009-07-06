c1d03e525d7eba4bded03dd886d7feda9ad885ae
lyonsam <lyonsam@cuban>
2009-07-06 14:00:03 -0500

    added example that tests shapes of array pointers

---
+++ TestSources/arrayPointerShape.f90
@@ -0,0 +1,11 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+! A simple program with an array pointer variable
+!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+program arrayPointerShape
+  real, dimension (:,:), pointer :: btn
+
+  print *, 'OK' 
+end program
