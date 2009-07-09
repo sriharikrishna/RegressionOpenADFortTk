ba57b06b48b6a868440404e06847f7901cead06d
m_utkej <m_utkej@utke2.site>
2009-07-08 22:33:35 -0500

    expanded test case

--- TestSources/doubleComplex.f90
+++ TestSources/doubleComplex.f90
@@ -1,7 +1,9 @@
 program p 
     double complex :: dc
-    double precision :: i1
+    double precision :: i1, p1, p2
     i1=1.0D0
     dc=cmplx(0.0D0, i1)
-    print *,dc
+    p1=real(dc)
+    p2=aimag(dc)
+    print *,dc, p1, p2
 end program
