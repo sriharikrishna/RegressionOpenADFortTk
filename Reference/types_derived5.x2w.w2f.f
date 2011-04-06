fe861313e08fe066851971fdd093e7d43944d461
utke <utke@linux-l3b2.site>
2011-04-06 17:21:58 -0500

    new test exercising changes for non-scalar type dereferences up to http://mercurial.mcs.anl.gov/ad/OpenADFortTk/rev/780f19a92dd6

---
+++ Reference/types_derived5.x2w.w2f.f
@@ -0,0 +1,56 @@
+
+      MODULE m
+      use w2f__types
+      IMPLICIT NONE
+      SAVE
+C
+C     **** Global Variables & Derived Type Definitions ****
+C
+      TYPE  T
+        REAL(w2f__4) X
+        REAL(w2f__4) Y
+      END TYPE
+      
+C
+C     **** Statements ****
+C
+      END MODULE
+
+      SUBROUTINE foo(X, Y)
+      use w2f__types
+      use m
+      IMPLICIT NONE
+C
+C     **** Parameters and Result ****
+C
+      REAL(w2f__4) X
+      REAL(w2f__4) Y
+C
+C     **** Local Variables and Functions ****
+C
+      TYPE (T) AT(1 : 2)
+C
+C     **** Statements ****
+C
+      AT(1)%X = X
+      AT(2)%Y = AT(1)%X
+      Y = AT(2)%Y
+      END SUBROUTINE
+
+      PROGRAM p
+      use w2f__types
+      IMPLICIT NONE
+C
+C     **** Local Variables and Functions ****
+C
+      EXTERNAL foo
+      REAL(w2f__4) X
+      REAL(w2f__4) Y
+C
+C     **** Statements ****
+C
+      X = 2.0
+      CALL foo(X, Y)
+      WRITE(*, *) Y
+      
+      END PROGRAM
